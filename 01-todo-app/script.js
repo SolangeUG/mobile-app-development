ca7af4d510ecd87260025b36bfcd95d26b4c4309
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-02-17 21:52:03 +0100

    Complete delete functionality

--- 01-todo-app/script.js
+++ 01-todo-app/script.js
@@ -13,6 +13,7 @@ const uncheckedCountSpan = document.getElementById('unchecked-count')
 // Globals
 this.counter = 0
 this.uncheckedCounter = this.counter
+this.randomNumbers = []
 
 /* ********************************
  * Create a new task and add it   *
@@ -48,9 +49,18 @@ function newTodo() {
  * ********************************/
 function getListItem(detail) {
 
+  // generate a random id for this list item
+  let id = Math.floor((Math.random() * 100) + 1)
+  // make sure this id is unique
+  while (this.randomNumbers.indexOf(id) > -1) {
+    id = Math.floor((Math.random() * 100) + 1)
+  }
+  this.randomNumbers.push(id)
+
   // create the checkbox element
   let checkbox = document.createElement('input')
   checkbox.setAttribute("type", "checkbox")
+  checkbox.setAttribute("id", "chk_" + id)
   checkbox.classList.add(classNames.TODO_CHECKBOX)
   checkbox.onclick = function() {
     let value = checkbox.checked ? -1 : 1
@@ -64,12 +74,21 @@ function getListItem(detail) {
 
   // create the delete button element
   let delButton = document.createElement('button')
-  delButton.innerHTML = "OK"
+  delButton.innerHTML = "DEL"
   delButton.classList.add(classNames.TODO_DELETE)
   delButton.onclick = function() {
-    // TODO: delete this task's list item
+    // update uncheckedCounter
+    // only if the task being deleted wasn't checked
+    let checkElement = document.getElementById("chk_" + id)
+    if (! checkElement.checked) {
+      updateUncheckedCounter(-1)
+    }
+    // update total list of tasks
     updateListCounter(-1)
-    updateUncheckedCounter(-1)
+
+    // delete this task's list item
+    let element = document.getElementById("li_" + id)
+    element.parentElement.removeChild(element)
   }
 
   // create the container element
@@ -81,6 +100,7 @@ function getListItem(detail) {
 
   // create the list-item element
   let listItem = document.createElement('li')
+  listItem.setAttribute("id", "li_" + id)
   listItem.classList.add(classNames.LIST_ITEM)
   listItem.append(container)
 
